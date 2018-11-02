f32176cade7361a971c149a166a5acbf57111f6b
Victor Tereschenko <sibvic@gmail.com>
2018-11-02 12:39:27 +0600

    Assert of indicator presence

--- LuaLint.cs
+++ LuaLint.cs
@@ -13,7 +13,39 @@ namespace fxlint
                 warnings.Add("Old version of ParseTime");
             if (ContainsOldTradingTimeCheck(code))
                 warnings.Add("Old version of trading time check (not via InRange)");
+            string[] missingIndicatorChecks = GetMissingIndicatorChecks(code);
+            foreach (var missingIndicatorCheck in missingIndicatorChecks)
+            {
+                warnings.Add("Missing indicator assert for " + missingIndicatorCheck);
+            }
             return warnings.ToArray();
+        }
+
+        static Regex indicatorCreatePattern = new Regex("indicators:create\\((?<indiName>[^,]+),");
+        private static string[] GetMissingIndicatorChecks(string code)
+        {
+            List<string> missingChecks = new List<string>();
+            var matches = indicatorCreatePattern.Matches(code);
+            foreach (Match match in matches)
+            {
+                var indicatorName = match.Groups["indiName"].Value;
+                if (IsStandardIndicator(indicatorName.Trim().Trim('"')))
+                    continue;
+                Regex indicatorAssertPattern = new Regex("assert\\(core\\.indicators:findIndicator\\(" + indicatorName + "\\) ~= nil");
+                if (!indicatorAssertPattern.IsMatch(code))
+                    missingChecks.Add(indicatorName);
+            }
+            return missingChecks.ToArray();
+        }
+
+        private static bool IsStandardIndicator(string indicatorName)
+        {
+            switch (indicatorName)
+            {
+                case "MVA":
+                    return true;
+            }
+            return false;
         }
 
         static Regex oldTradingTimeCheckPattern = new Regex("if not\\(now ?>= ?OpenTime(\n\r)?[^a]*and now ?<= ?CloseTime\\)");
