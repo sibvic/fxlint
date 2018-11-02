2b5110815a310f822fcef64c6f5987d4fbf2d32d
Victor Tereschenko <sibvic@gmail.com>
2018-11-02 10:34:59 +0600

    Empty project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fxlint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
