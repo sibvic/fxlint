1f995eaeb3cc1d8634aa695bb3137917c43bf394
Victor Tereschenko <sibvic@gmail.com>
2019-02-05 13:40:23 +0600

    tests

---
+++ fxlint_tests/InRange.cs
@@ -0,0 +1,94 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fxlint.Cases;
+
+namespace fxlint_tests
+{
+    [TestClass]
+    public class InRange
+    {
+        private const string inRangeSnippet = @"if not(now >= OpenTime
+	and now <= CloseTime)
+	then";
+        private const string inRangeSnippet2 = @"       if not(now >= OpenTime
+	  and now <= CloseTime)
+	  and not ManageExit
+	  then            ";
+        readonly string noInrangeUseSnippet = @"some code";
+        readonly string inRangeUseSnippet = @"-- NG: create a function to parse time
+function ParseTime(time)
+    local Pos = string.find(time, "":"");
+    if Pos == nil then
+        return nil, false;
+            end
+            local h = tonumber(string.sub(time, 1, Pos - 1));
+            time = string.sub(time, Pos + 1);
+            Pos = string.find(time, "":"");
+            if Pos == nil then
+        return nil, false;
+            end
+            local m = tonumber(string.sub(time, 1, Pos - 1));
+            local s = tonumber(string.sub(time, Pos + 1));
+            return (h / 24.0 + m / 1440.0 + s / 86400.0),                          --time in ole format
+                 ((h >= 0 and h< 24 and m >= 0 and m< 60 and s >= 0 and s< 60) or(h == 24 and m == 0 and s == 0)); --validity flag
+ end
+
+      if not InRange(now, OpenTime, CloseTime) then  
+";
+
+        [TestMethod]
+        public void InRangePresentDetect()
+        {
+            InRangeUse check = new InRangeUse();
+            var warnings = check.GetWarnings(inRangeUseSnippet);
+            Assert.AreEqual(1, warnings.Length);
+        }
+
+        [TestMethod]
+        public void InRangePresentFix()
+        {
+            InRangeUse check = new InRangeUse();
+            var fixedCode = check.Fix(inRangeUseSnippet);
+            Assert.AreEqual(true, fixedCode.Contains("function InRange("));
+        }
+
+        [TestMethod]
+        public void InRangePresentFixNoNeed()
+        {
+            InRangeUse check = new InRangeUse();
+            var fixedCode = check.Fix(noInrangeUseSnippet);
+            Assert.AreEqual(noInrangeUseSnippet, fixedCode);
+        }
+
+        [TestMethod]
+        public void OldTradingTimeCheckDetect()
+        {
+            OldTradingTimeCheck check = new OldTradingTimeCheck();
+            var warnings = check.GetWarnings(inRangeSnippet);
+            Assert.AreEqual(1, warnings.Length);
+        }
+
+        [TestMethod]
+        public void OldTradingTimeCheckDetect2()
+        {
+            OldTradingTimeCheck check = new OldTradingTimeCheck();
+            var warnings = check.GetWarnings(inRangeSnippet2);
+            Assert.AreEqual(1, warnings.Length);
+        }
+
+        [TestMethod]
+        public void OldTradingTimeCheckFix()
+        {
+            OldTradingTimeCheck check = new OldTradingTimeCheck();
+            var fixedCode = check.Fix(inRangeSnippet);
+            Assert.AreEqual(true, fixedCode.Contains(" not InRange(now, OpenTime, CloseTime) "));
+        }
+
+        [TestMethod]
+        public void OldTradingTimeCheckFix2()
+        {
+            OldTradingTimeCheck check = new OldTradingTimeCheck();
+            var fixedCode = check.Fix(inRangeSnippet2);
+            Assert.AreEqual(true, fixedCode.Contains(" not InRange(now, OpenTime, CloseTime) "));
+        }
+    }
+}
---
+++ fxlint_tests/MissingIndicator.cs
@@ -0,0 +1,41 @@
+﻿using fxlint.Cases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fxlint_tests
+{
+    [TestClass]
+    public class MissingIndicator
+    {
+        const string presentSnippet = @"    assert(core.indicators:findIndicator(M[i]) ~= nil, M[i] .. "" indicator must be installed"");
+        Row[i]=core.indicators:create(M[i], source[IN[i]], F[i]);";
+        const string presentSnippet2 = @"    assert(core.indicators:findIndicator(   instance.parameters:getString(""Method"" .. i)) ~= nil,    instance.parameters:getString(""Method"" .. i) .. "" indicator must be installed"");
+        MA[i] = core.indicators:create(instance.parameters:getString(""Method"" .. i), Source[instance.parameters:getString(""Price""..i)],  instance.parameters:getInteger(""Period"" .. i)  );";
+        //    assert(core.indicators:findIndicator("ICHIMOKU + DAILY-CANDLE_X + HULL-MA_X + MACD") ~= nil, "ICHIMOKU + DAILY-CANDLE_X + HULL-MA_X + MACD" .. " indicator must be installed");
+//        indi = core.indicators:create("ICHIMOKU + DAILY-CANDLE_X + HULL-MA_X + MACD", trading_logic.MainSource.close, keh);
+
+        [TestMethod]
+        public void MissingCheckPresent()
+        {
+            MissingIndicatorCheck check = new MissingIndicatorCheck();
+            var warnings = check.GetWarnings(presentSnippet);
+            Assert.AreEqual(0, warnings.Length);
+
+            var warnings2 = check.GetWarnings(presentSnippet2);
+            Assert.AreEqual(0, warnings2.Length);
+        }
+
+        [TestMethod]
+        public void MissingFixPresent()
+        {
+            MissingIndicatorCheck check = new MissingIndicatorCheck();
+            var fixedCode = check.Fix(presentSnippet);
+            Assert.AreEqual(presentSnippet, fixedCode);
+
+            var fixedCode2 = check.Fix(presentSnippet2);
+            Assert.AreEqual(presentSnippet2, fixedCode2);
+        }
+    }
+}
